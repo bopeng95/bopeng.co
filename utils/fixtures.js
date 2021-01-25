084b05f3f4a976fe8ca2c3982523632d1adc275e
Bo Peng <pb0928@gmail.com>
2021-01-24 20:08:00 -0800

    added projects

--- utils/fixtures.js
+++ utils/fixtures.js
@@ -8,11 +8,30 @@ import { MdCheckCircle } from 'react-icons/md';
 import { IoEllipsisHorizontalCircleSharp } from 'react-icons/io5';
 
 const langTags = {
+  bash: { tag: 'bash', color: 'gray' },
   js: { tag: 'javascript', color: 'yellow' },
+  cheerio: { tag: 'cheerio', color: 'yellow' },
+  react: { tag: 'react', color: 'blue' },
+  discord: { tag: 'discord', color: 'blue' },
+  d3: { tag: 'd3', color: 'orange' },
+  redis: { tag: 'redis', color: 'red' },
+  electron: { tag: 'electron', color: 'purple' },
   node: { tag: 'node', color: 'green' },
+  npm: { tag: 'npm', color: 'teal' },
 };
 
-const { js, node } = langTags;
+const {
+  js,
+  node,
+  bash,
+  npm,
+  react,
+  d3,
+  electron,
+  redis,
+  discord,
+  cheerio,
+} = langTags;
 
 export const nav = ['projects', 'resume'];
 
@@ -57,24 +76,52 @@ export const pLegend = [
 
 export const projects = [
   {
-    name: 'project 1',
+    name: 'dotfiles',
     // link: '',
+    Icon: IoEllipsisHorizontalCircleSharp,
+    iconColor: 'yellow.400',
+    tech: [bash],
+  },
+  {
+    name: 'ValPatches',
+    link: 'https://github.com/bopeng95/ValPatches',
+    Icon: IoEllipsisHorizontalCircleSharp,
+    iconColor: 'yellow.400',
+    tech: [node, discord],
+  },
+  {
+    name: 'valorant patch api',
+    link: 'https://valapi.vercel.app/patches',
     Icon: MdCheckCircle,
     iconColor: 'green.400',
-    tech: [js, node],
+    tech: [node, cheerio],
   },
   {
-    name: 'project 2',
-    // link: '',
+    name: 'novaql - desktop',
+    link: 'https://github.com/nova-introspection/novaql',
     Icon: IoEllipsisHorizontalCircleSharp,
     iconColor: 'yellow.400',
-    tech: [js, node],
+    tech: [react, node, d3, redis, electron],
   },
   {
-    name: 'project 3',
-    // link: '',
-    Icon: IoEllipsisHorizontalCircleSharp,
-    iconColor: 'yellow.400',
-    tech: [js, node],
+    name: 'novaql - web',
+    link: 'https://github.com/nova-introspection/Nova',
+    Icon: MdCheckCircle,
+    iconColor: 'green.400',
+    tech: [react, node, d3, redis],
+  },
+  {
+    name: '@bopeng95/wbpk',
+    link: 'https://www.npmjs.com/package/@bopeng95/wbpk',
+    Icon: MdCheckCircle,
+    iconColor: 'green.400',
+    tech: [js, react, node, npm],
+  },
+  {
+    name: '@bopeng95/updater',
+    link: 'https://github.com/bopeng95/updater',
+    Icon: MdCheckCircle,
+    iconColor: 'green.400',
+    tech: [js, node, npm],
   },
 ];
